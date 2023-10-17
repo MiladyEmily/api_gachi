baa1bfc0443003c66daf78a8dad2400579706945
Emilia Chernova <emiliia.chernova@yandex.ru>
2023-10-17 19:15:36 +0300

    добавила модели имени и гач

--- api_gachi/gachi/models.py
+++ api_gachi/gachi/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseName(models.Model):
+    """
+    Базовое полное имя.
+    Связано с вариациями имени один-ко-многим
+    """
+    basename = models.CharField('Базовое имя', max_length=200, unique=True)
+    slug = models.SlugField('Уникальный слаг', unique=True, max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Name(models.Model):
+    """
+    Вариация имени.
+    Связано с базовым именем и гачами один-ко-многим.
+    """
+    name = models.CharField('Имя', max_length=200, unique=True)
+    slug = models.SlugField('Уникальный слаг', unique=True, max_length=200)
+    basename = models.ForeignKey(
+        BaseName,
+        on_delete=models.CASCADE,
+        related_name='name_variants',
+        verbose_name='Базовое имя',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Gachi(models.Model):
+    """
+    Модель гачи.
+    Связано с базовым именем один-ко многим
+    """
+    gachi = models.CharField('Гача', max_length=200)
+    name = models.ForeignKey(
+        Name,
+        on_delete=models.CASCADE,
+        related_name='gachies',
+        verbose_name='Имя',
+    )
